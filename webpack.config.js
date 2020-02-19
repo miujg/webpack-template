dea8ee4a2693d949194d0e910ecae401ec22d6cc
miujg <1066647329@qq.com>
2020-02-19 16:36:16 +0800

    部分基本配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const path = require('path'),
+    HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     //模式
@@ -6,7 +7,22 @@ module.exports = {
     // 入口
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.js', // 可以配置hash: bundle.[hash:8].js
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    // webpack serve 配置
+    devServer: {
+        port: 3000,
+        // 进度条
+        progress: true,
+        contentBase: './dist',
+        compress: true
+    },
+    // 配置插件
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }) 
+    ]
 }
